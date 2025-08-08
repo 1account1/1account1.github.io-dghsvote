a62df37750457eb6ed7335760bfd2ce7e19e5e94
1account1 <97660601+1account1@users.noreply.github.com>
2025-08-08 10:03:07 +0000

    nice weather

---
+++ barr.js
@@ -0,0 +1,4 @@
+const barr = '<span class="bar"><a href="gongyak.html">후보자 공약</a></span><span class="bar"><a href="toron.html">후보자 인터뷰</a></span><span class="bar"><a href="roadmap.html">선거 일정 로드맵</a></span><span class="bar"><a href="yeoronjosa.html">여론조사 톺아보기</a></span><span class="bar"><a href="exit.html">출구조사</a></span>';
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('barr').innerHTML = barr;
+})
