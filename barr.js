eff249a9f1fcc26d8f9d7ac41971b8cdb359419c
1account1 <97660601+1account1@users.noreply.github.com>
2025-08-10 07:13:25 +0000

    redesigned

--- barr.js
+++ barr.js
@@ -1,4 +1,4 @@
-const barr = '<span class="bar"><a href="gongyak.html">후보자 공약</a></span><span class="bar"><a href="toron.html">후보자 인터뷰</a></span><span class="bar"><a href="roadmap.html">선거 일정 로드맵</a></span><span class="bar"><a href="yeoronjosa.html">여론조사 톺아보기</a></span><span class="bar"><a href="exit.html">출구조사</a></span>';
+const barr = '<a href="gongyak.html"><span class="bar" id="aa">후보자 공약</span></a><a href="toron.html"><span class="bar" id="bb">후보자 인터뷰</span></a><a href="roadmap.html"><span class="bar" id="cc">선거 일정 로드맵</span></a><a href="yeoronjosa.html"><span class="bar" id="dd">여론조사 톺아보기</span></a><a href="exit.html"><span class="bar" id="ee">출구조사</span></a>';
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('barr').innerHTML = barr;
 })
--- dday.js
+++
@@ -1,6 +0,0 @@
-const today = new Date();
-const day = today.getDate();
-let dday = 28 - day;
-document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById('dday').innerText = "D - " + dday;
-})
