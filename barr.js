3f954a28bd72f1678cc539a7426d7c6176a85163
1account1 <97660601+1account1@users.noreply.github.com>
2025-08-25 17:09:55 +0900

    Update barr.js

--- barr.js
+++ barr.js
@@ -1,4 +1,4 @@
-const barr = '<a href="gongyak.html"><span class="bar" id="aa">후보자 공약</span></a><a href="toron.html"><span class="bar" id="bb">후보자 인터뷰</span></a><a href="roadmap.html"><span class="bar" id="cc">선거 일정 로드맵</span></a><a href="roadmap.html"><span class="bar" id="ff">후보자 토론</span></a><a href="yeoronjosa.html"><span class="bar" id="dd">여론조사</span></a><a href="exit.html"><span class="bar" id="ee">출구조사</span></a>';
+const barr = '<a href="gongyak.html"><span class="bar" id="aa">후보자 공약</span></a><a href="toron.html"><span class="bar" id="bb">후보자 인터뷰</span></a><a href="roadmap.html"><span class="bar" id="cc">선거 일정 로드맵</span></a><a href="toronvid.html"><span class="bar" id="ff">후보자 토론</span></a><a href="yeoronjosa.html"><span class="bar" id="dd">여론조사</span></a><a href="exit.html"><span class="bar" id="ee">출구조사</span></a>';
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('barr').innerHTML = barr;
 })
